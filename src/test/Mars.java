8ac5b677ac17c82212e05c724ceb2dc852f7b7fc
[denizbabayaka] <[denizbabayaka@gmail.com]>
2020-02-05 09:28:37 -0500

    stash after changes

--- src/test/Mars.java
+++ src/test/Mars.java
@@ -4,7 +4,11 @@ public class Mars {
 
 	public static void main(String[] args) {
     
+		System.out.println("first stash");
 		
+		System.out.println("second stash");
+		
+		System.out.println("third stash");
 		
 		
 		
