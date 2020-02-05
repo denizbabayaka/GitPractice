04fe05a3be65da06c904f70748a0861101cd8110
[denizbabayaka] <[denizbabayaka@gmail.com]>
2020-02-05 08:51:26 -0500

    third commit

--- src/test/Mars.java
+++ src/test/Mars.java
@@ -3,20 +3,7 @@ package test;
 public class Mars {
 
 	public static void main(String[] args) {
-<<<<<<< Updated upstream
-		// TODO Auto-generated method stub
-		
-		
-		
-		
-		System .out.println("Hello");//This will create hello
 
-=======
-		
-		System.out.println("Hello World");
-		System.out.println("Hell");
-		
->>>>>>> Stashed changes
 	}
 
 }
